bd2542aa27ec4a5866a7b3929f83c6f7794dfa33
wakchu <stfnbrdn@gmail.com>
2025-11-21 17:33:08 +0100

    Refacotring Azienda.java

---
+++ Azienda/src/Dipendente.java
@@ -0,0 +1,34 @@
+public class Dipendente {
+    private String nome;
+    private String cognome;
+    private Ruolo Ruolo;
+    private int ral;
+
+    public Dipendente(String nome, String cognome,Ruolo Ruolo, int ral) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.Ruolo = Ruolo;
+        this.ral = ral;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getCognome() {
+        return cognome;
+    }
+    public int getRal() {
+        return ral;
+    }
+    public Ruolo getRuolo() {
+        return Ruolo;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+    public void setRal(int ral) {
+        this.ral = ral;
+    }
+}
---
+++ Azienda/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+            public interface Filtra<Dipendente> {
+            boolean filtra(Dipendente d);
+        }
+    public static void main(String[] args) {
+
+        List<Dipendente> dipendenti = Arrays.asList(
+                new Dipendente("Mario", "Rossi", Ruolo.DIPENDENTE, 30000),
+                new Dipendente("Luigi", "Verdi", Ruolo.AMMINISTRATORE, 50000),
+                new Dipendente("Giovanni", "Bianchi", Ruolo.DIPENDENTE, 25000),
+                new Dipendente("Anna", "Neri", Ruolo.AMMINISTRATORE, 60000)
+        );
+        Filtra<Dipendente> filtra30k = (d) -> d.getRal() > 30000;
+        Filtra<Dipendente> filtraAmministratori = (d) -> d.getRuolo() == Ruolo.AMMINISTRATORE;
+        Filtra<Dipendente> filtraDipendenti = (d) -> d.getRuolo() == Ruolo.DIPENDENTE;
+        List<Dipendente> dipendentiFiltrati = dipendenti.stream()
+                .filter(filtra30k::filtra)
+                .sorted(Comparator.comparing(Dipendente::getRal))
+                .toList();
+
+        dipendentiFiltrati.forEach(d -> System.out.println(d.getNome() + " " + d.getCognome() + " " + d.getRal()));
+
+       dipendenti.stream()
+               .max(Comparator.comparing(Dipendente::getRal))
+               .ifPresent(d -> System.out.println("Il dipendente con il RAL più alto è: " + d.getNome() + " " + d.getCognome() + " " + d.getRal()));
+        dipendenti.stream()
+                .filter(filtraAmministratori::filtra)
+                .forEach(d -> System.out.println(d.getNome() + " " + d.getCognome() + " " + d.getRal()));
+        dipendenti.stream()
+                .filter(filtraDipendenti::filtra)
+                .forEach(d -> System.out.println(d.getNome() + " " + d.getCognome() + " " + d.getRal()));
+    }
+}
